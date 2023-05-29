faef971d6c67b37c9fd2f320d4ff5a852136ff62
Tiger101986 <kanharobopha@yahoo.com>
2023-05-29 08:54:08 -0700

    working on post.vue

--- frontend/src/components/post.vue
+++ frontend/src/components/post.vue
@@ -1,20 +1,10 @@
 <template>
   <div class="posts" method="post">
-    <input
-      type="text"
-      name="contents"
-      placeholder="Enter Contents"
-      v-model="posts.content"
-    />
+    <input type="text" name="contents" placeholder="Enter Contents" v-model="post.contents" />
     <div class="file">
-      <input
-        style="display: none"
-        name="file" 
-        type="file" 
-        ref="fileInput" 
-        @change="onSelectFile">
+      <input style="display: none" name="file" type="file" ref="fileInput" @change="onSelectFile">
       <button @click="$refs.fileInput.click()"> Choose Image </button>
-    </div>  
+    </div>
     <button class="btn" @click.prevent="postContents" type="submit">Post Content!</button>
   </div>
 </template>
@@ -24,80 +14,78 @@ export default {
   name: "PostUp",
   data() {
     return {
-      posts: {
+      post: {
         userId: "",
-        content: "",
-        imageUrl: "",
+        contents: "",
+        media: null,
       },
     };
   },
   methods: {
     onSelectFile(e) {
-      this.posts.imageUrl = e.target.files[0];
+      this.post.media = e.target.files[0];
     },
     // Create post 
-    postContents() { 
-      const formData = new FormData();
-      formData.append("Posts", this.posts.content);
-      formData.append("image", this.posts.imageUrl);
-      const token = JSON.parse(localStorage.getItem("users-info")).token;
-      if( formData ) {
+    postContents() {
+      const { userId, token } = JSON.parse(localStorage.getItem("users-info"));
+      if (this.post.media) {
+        const formData = new FormData();
+        formData.append("post", JSON.stringify({ contents: this.post.contents, userId }));
+        formData.append("image", this.post.media);
         fetch("http://localhost:3000/api/posts", {
           method: 'post',
-          headers: { 
+          headers: {
             "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json" },
-          body: JSON.stringify({
-            userId: this.posts.userId,
-            /* content: this.posts.content, */
-          }), formData
+            'Accept': 'application/json',
+            "Content-Type": "multipart/form-data"
+          },
+          body: formData
         })
           .then((response) => response.json())
           .then((data) => {
-            localStorage.setItem("posts-info", JSON.stringify(data));  
-            this.$router.push({name: 'Home'}); //router to home page
+            this.$router.push({ name: 'Home' }); //router to home page
             console.log(data);
           })
       } else {
-          fetch("http://localhost:3000/api/posts", {
-            method: 'post',
-            headers: { 
-              "Authorization": `Bearer ${token}`,
-              "Content-Type": "application/json" },
-            body: JSON.stringify({
-              userId: this.posts.userId,
-              content: this.posts.content,
-            })
+        fetch("http://localhost:3000/api/posts", {
+          method: 'post',
+          headers: {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            userId,
+            contents: this.post.contents,
           })
-            .then((response) => {
-              if ( response.status !== 201) {
-                throw response.status;
-              }else {
-                return response.json()
-              }
-            })
-            .then((data) => {
-              localStorage.setItem("posts-info", JSON.stringify(data));  
-              this.$router.push({name: 'Home'}); //router to home page
-              console.log(data);
-            }).catch( error => { 
-              console.log(error); 
-            })
+        })
+          .then((response) => {
+            if (response.status !== 201) {
+              throw response.status;
+            } else {
+              return response.json()
+            }
+          })
+          .then((data) => {
+            this.$router.push({ name: 'Home' }); //router to home page
+            console.log(data);
+          }).catch(error => {
+            console.log(error);
+          })
       }
-      
-        /* const orderData = {
-            method: 'post',
-            headers: { "Content-Type": "application/json", "Authorization": `Bearer ${this.token}`, },
-            body: JSON.stringify({}),
-        };
-        fetch('http://localhost:3000/api/products/order', orderData)
-            .then((response) => response.json())
-            .then((data) => {
-                let confirmationUrl = './confirmation.html?orderedId=' + data.orderId;
-                window.location.href = confirmationUrl;
-                localStorage.clear();
-            })
-            .catch(error => console.log(error));   */
+
+      /* const orderData = {
+          method: 'post',
+          headers: { "Content-Type": "application/json", "Authorization": `Bearer ${this.token}`, },
+          body: JSON.stringify({}),
+      };
+      fetch('http://localhost:3000/api/products/order', orderData)
+          .then((response) => response.json())
+          .then((data) => {
+              let confirmationUrl = './confirmation.html?orderedId=' + data.orderId;
+              window.location.href = confirmationUrl;
+              localStorage.clear();
+          })
+          .catch(error => console.log(error));   */
     },
   },
 };
@@ -111,16 +99,19 @@ export default {
   margin: auto 30px;
   padding: 30px;
 }
+
 div input {
   width: auto;
   height: 300px;
   text-align: center;
   /* border: 1px solid blue; */
 }
+
 ::placeholder {
   font-size: 20px;
 }
-.file button{
+
+.file button {
   width: 150px;
   height: 30px;
   padding: 10px auto 20px;
@@ -129,6 +120,7 @@ div input {
   /* bottom: 475px; */
   left: 68px;
 }
+
 div .btn {
   position: absolute;
   left: 250px;
@@ -137,7 +129,7 @@ div .btn {
   height: 30px;
   margin-top: 20px;
   /* background-color: white; */
- color: blue;
+  color: blue;
   /* border: 1px solid blue; */
 }
 </style>
